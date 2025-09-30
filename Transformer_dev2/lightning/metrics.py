07348bb0bcf5000ccc3c6c1881d236f0fec31195
root <haiyang@126.com>
2025-09-30 04:22:42 +0000

    Remove Transformer_dev2 directory

--- Transformer_dev2/lightning/metrics.py
+++
@@ -1,2 +0,0 @@
-# This file is currently empty as all custom trading metrics have been removed.
-# The project now uses standard torchmetrics from the torchmetrics library.
--- Transformer_dev2/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .logging_utils import setup_logging
-
-__all__ = [
-    # Logging
-    'setup_logging',
-]
